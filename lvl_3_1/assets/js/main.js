8650d1224affe336c437abf7a92dd7127df207d8
Rizvan555 <rizvan84@gmx.de>
2023-03-29 15:01:24 +0200

    added lvl_3_1

---
+++ lvl_3_1/assets/js/main.js
@@ -0,0 +1,39 @@
+const cat = document.querySelector("#cat");
+
+let leftPosition = 0;
+let catWalkInterval;
+
+function catWalk() {
+  catWalkInterval = setInterval(() => {
+    leftPosition += 10;
+    cat.style.left = leftPosition + "px";
+    if (leftPosition < window.innerWidth) {
+      clearInterval(catWalkInterval);
+      cat.style.left = leftPosition + "px";
+    }
+  }, 500);
+}
+
+function pause() {
+  clearInterval(catWalkInterval);
+}
+
+function turn() {
+  catWalkInterval = setInterval(() => {
+    cat.style.transform = "rotateY(180deg)";
+    leftPosition = 0;
+    cat.style.left = leftPosition + "px";
+    clearInterval(catWalkInterval);
+  }, 200);
+}
+
+function catSpeed() {
+  catWalkInterval = setInterval(() => {
+    leftPosition += 100;
+    cat.style.left = leftPosition + "px";
+    if (leftPosition < window.innerWidth) {
+      clearInterval(catWalkInterval);
+      cat.style.left = leftPosition + "px";
+    }
+  }, 100);
+}
